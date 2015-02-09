8ceb8c074327ecd4be630018c7643f84e26f1b08
Ken <aenkil@gmail.com>
2015-02-09 23:08:55 +1100

    Include the extra secrets.js.

---
+++ server/config/secrets.js
@@ -0,0 +1,8 @@
+/** Important **/
+/** You should not be committing this file to GitHub **/
+
+module.exports = {
+  // Find the appropriate database to connect to, default to localhost if not found.
+  db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/ReactWebpackNode',
+  sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here'
+};
