ac24993b64afcf8fd800eb54dcc5a12525513fa5
Simon Knox <simon.knox@kogan.com.au>
2015-04-27 20:06:57 +1000

    revert unused require

--- app/app.js
+++ app/app.js
@@ -3,6 +3,4 @@ var IsomorphicRouterRenderer = require('./utils/IsomorphicRouterRenderer');
 var alt = require('./alt');
 var routes = require('./routes');
 
-var css = require('!css!sass!./scss/main.scss');
-
 module.exports = IsomorphicRouterRenderer(alt, routes);
