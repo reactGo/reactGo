750497d43b604b04a5c59db2234b28e8daaef64e
Ken <aenkil@gmail.com>
2015-04-28 21:57:17 +1000

    minor changes for css and WIP for saving the google auth model.

--- server/config/passport/google.js
+++ server/config/passport/google.js
@@ -12,26 +12,27 @@ module.exports = new GoogleStrategy({
 	clientID: secrets.google.clientID,
 	clientSecret: secrets.google.clientSecret,
 	callbackURL: secrets.google.callbackURL
-}, function(accessToken, refreshToken, profile, done) {
-	var options = {
-		criteria: { 'google.id': profile.id }
-	};
-	User.load(options, function(err, user) {
-		if (err) return done(err);
-		if (!err) {
-			console.log(profile);
-			user = new User({
-				name: profile.displayName,
-				username: profile.username,
-				provider: 'google',
-				google: profile._json
-			});
-			user.save(function(err) {
-				if(err) console.log(err);
-				return done(err, user);
-			});
-		} else {
-			return done(err, user);
-		}
-	})
+},  function(accessToken, refreshToken, profile, done) {
+	    var options = {
+	      criteria: { 'google.id': profile.id }
+	    };
+	    User.load(options, function (err, user) {
+	      if (err) return done(err);
+	      console.log(user);
+	      if (!user) {
+	        user = new User({
+	          name: profile.displayName,
+	          email: profile.emails[0].value,
+	          username: profile.username,
+	          provider: 'google',
+	          google: profile._json
+	        });
+	        user.save(function (err) {
+	          if (err) console.log(err);
+	          return done(err, user);
+	        });
+	      } else {
+	        return done(err, user);
+	      }
+	    });
 });
