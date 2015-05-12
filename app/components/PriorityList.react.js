1ccee0c53b1265f23e040079c384fe8ec5d4c1ca
Ken <aenkil@gmail.com>
2015-05-12 10:29:55 +1000

    Adding extra components

---
+++ app/components/PriorityList.react.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var PriorityList = React.createClass({
+	render: function() {
+		return (
+			<h1>I am a List</h1>
+		);
+	}
+});
+
+module.exports = PriorityList;
---
+++ app/components/Profile.react.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Profile = React.createClass({
+	render: function() {
+		return (
+			<h1>I am a profile</h1>
+		);
+	}
+});
+
+module.exports = Profile;
