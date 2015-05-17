bc3dc3f7b576c8c2f22abfde5f282d42c974f156
Ken <aenkil@gmail.com>
2015-05-17 21:57:46 +1000

    Adding additional comments and removing html-loader as a dependency

--- app/utils/initInitialImages.js
+++ app/utils/initInitialImages.js
@@ -1,6 +1,9 @@
 /*
 	Requiring images. Webpack will copy these images into the correct directory to be used
 	by the template.
+
+	Note: I am still experimenting with including images with webpack. Was thinking of using html-loader,
+	but it did not seem to fit in with the webpack + express setup we have.
  */
 require('../images/apple-touch-icon-precomposed.png');
 require('../images/chrome-touch-icon-192x192-precomposed.png');
