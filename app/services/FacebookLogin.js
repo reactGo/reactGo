e4cc610440b4fc4d0df4df56e3e437b9e9764134
Ken Fehling <ken@androidideas.org>
2015-06-01 23:16:01 -0400

    checking for window

--- app/services/FacebookLogin.js
+++ app/services/FacebookLogin.js
@@ -49,43 +49,46 @@ function login() {
 }
 
 function fbInit() {
-	window.fbAsyncInit = function() {
-		console.log("fbAsyncInit");
-		FB.init({
-			appId      : '160584927341574',
-			cookie     : true,  // enable cookies to allow the server to access
-			                    // the session
-			xfbml      : true,  // parse social plugins on this page
-			version    : 'v2.2' // use version 2.2
-		});
+    if (typeof window !== "undefined") {
+        window.fbAsyncInit = function () {
+            console.log("fbAsyncInit");
+            FB.init({
+                appId: '160584927341574',
+                cookie: true,  // enable cookies to allow the server to access
+                               // the session
+                xfbml: true,  // parse social plugins on this page
+                version: 'v2.2' // use version 2.2
+            });
 
-		// Now that we've initialized the JavaScript SDK, we call
-		// FB.getLoginStatus().  This function gets the state of the
-		// person visiting this page and can return one of three states to
-		// the callback you provide.  They can be:
-		//
-		// 1. Logged into your app ('connected')
-		// 2. Logged into Facebook, but not your app ('not_authorized')
-		// 3. Not logged into Facebook and can't tell if they are logged into
-		//    your app or not.
-		//
-		// These three cases are handled in the callback function.
+            // Now that we've initialized the JavaScript SDK, we call
+            // FB.getLoginStatus().  This function gets the state of the
+            // person visiting this page and can return one of three states to
+            // the callback you provide.  They can be:
+            //
+            // 1. Logged into your app ('connected')
+            // 2. Logged into Facebook, but not your app ('not_authorized')
+            // 3. Not logged into Facebook and can't tell if they are logged into
+            //    your app or not.
+            //
+            // These three cases are handled in the callback function.
 
-		FB.getLoginStatus(function(response) {
-			statusChangeCallback(response);
-		});
-	};
+            FB.getLoginStatus(function (response) {
+                statusChangeCallback(response);
+            });
+        };
 
-	// Load the SDK asynchronously
-	(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) {
-			return;
-		}
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_US/sdk.js";
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
+        // Load the SDK asynchronously
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {
+                return;
+            }
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    }
 }
 
 module.exports = {
