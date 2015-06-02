7b8f36d05c50e8059cc2f7dcb9a3d99a6ee51d93
Ken Fehling <ken@androidideas.org>
2015-06-01 22:30:43 -0400

    Isomorphism is interesting

--- app/services/FacebookLogin.js
+++ app/services/FacebookLogin.js
@@ -75,21 +75,21 @@ function fbInit() {
 			statusChangeCallback(response);
 		});
 	};
-}
 
-// Load the SDK asynchronously
-(function(d, s, id) {
-var js, fjs = d.getElementsByTagName(s)[0];
-if (d.getElementById(id)) {
-	return;
-}
-js = d.createElement(s); js.id = id;
-js.src = "//connect.facebook.net/en_US/sdk.js";
-fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+	// Load the SDK asynchronously
+	(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) {
+			return;
+		}
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
 
-module.exports = {
-	fbInit: fbInit,
-	addLoginListener: addLoginListener,
-	login: login
-};
+	module.exports = {
+		fbInit: fbInit,
+		addLoginListener: addLoginListener,
+		login: login
+	};
+}
