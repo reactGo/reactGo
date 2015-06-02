867699252489fad909a95f05e97307d5f379b6d6
Ken Fehling <ken@androidideas.org>
2015-06-01 21:46:32 -0400

    Trying FB on client

--- app/components/App.react.js
+++ app/components/App.react.js
@@ -1,4 +1,7 @@
+'use strict';
+var $ = require('jquery');
 var React = require('react');
+var FacebookLogin = require('../services/FacebookLogin');
 var RouteHandler = require('react-router').RouteHandler;
 var Navigation = require('../components/Navigation.react');
 
@@ -6,14 +9,29 @@ require('../utils/initInitialImages');
 require('../scss/main.scss');
 
 var App = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <Navigation />
-        <RouteHandler />
-      </div>
-    );
-  }
+    componentWillMount: function() {
+        this.state = {
+            user: null
+        };
+        FacebookLogin.addLoginListener(function(accessToken) {
+            var callback = function(response) {
+                this.setState({ user: response });
+            };
+            var data = { accessToken: accessToken };
+            //$.post('https://localhost:3000/user', data, callback.bind(this), 'json');
+        }.bind(this));
+        FacebookLogin.fbInit();
+    },
+
+    render: function() {
+        return (
+            <div>
+                <Navigation />
+                <div><a onClick={FacebookLogin.login}>Login</a></div>
+                <RouteHandler />
+            </div>
+        );
+    }
 });
 
 module.exports = App;
