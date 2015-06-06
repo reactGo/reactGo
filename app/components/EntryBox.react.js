bd2abf29b1147056e7135759c577cf9591097151
Ken <aenkil@gmail.com>
2015-06-06 17:28:03 +1000

    - Adding some simple css in to make things look a bit better before the ES6 refactor - Moving a bit of the code to follow better conventions

--- app/components/EntryBox.react.js
+++ app/components/EntryBox.react.js
@@ -1,38 +1,10 @@
 var React = require('react');
 var TopicActions = require('../actions/TopicActions');
 var TopicTextInput = require('./TopicTextInput.react');
-// var Statistics = require('./Statistics.react');
-// var AnimationMixin = require('../mixins/AnimationMixin');
 
 var EntryBox = React.createClass({
   propTypes: {
     topic: React.PropTypes.string
-  },
-  // mixins: [AnimationMixin],
-  // getInitialState: function() {
-  //   return {
-  //     opaque: false
-  //   };
-  // },
-  // componentDidMount: function() {
-  //   this.setState({
-  //     opaque : true
-  //   })
-  // },
-  render: function() {
-    // <Statistics topTopic={this.props.topTopic} topStat={this.props.topStat} />
-    // <h1 className={classnames({
-    //       'opaque--true': this.state.opaque,
-    //       'opaque--false': !this.state.opaque
-    //     })}>
-    // {this.createTextTransition(text)}
-    // </h1>
-    return (
-      <div className="entrybox">
-        <h1 className="entrybox__header">Vote for your top hack idea</h1>
-        <TopicTextInput className="entrybox__input" value={this.props.topic} placeholder="What's yer fav idea?" onChange={this._onChange} onSave={this._onSave} />
-      </div>
-    );
   },
 
   /**
@@ -50,6 +22,16 @@ var EntryBox = React.createClass({
    */
   _onChange: function(text) {
     TopicActions.typing(text);
+  },
+
+  render: function() {
+
+    return (
+      <div className="entrybox">
+        <h1 className="entrybox__header">Vote for your top hack idea</h1>
+        <TopicTextInput className="entrybox__input" value={this.props.topic} placeholder="What's yer fav idea?" onChange={this._onChange} onSave={this._onSave} />
+      </div>
+    );
   }
 
 });
