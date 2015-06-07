50aee78697360030cda77aba547d58636c2e1132
Ken <aenkil@gmail.com>
2015-06-08 00:38:00 +1000

    - Quick surprise with new ninja favicons!

--- app/utils/initInitialImages.js
+++ app/utils/initInitialImages.js
@@ -5,6 +5,6 @@
 	Note: I am still experimenting with including images with webpack. Was thinking of using html-loader,
 	but it did not seem to fit in with the webpack + express setup we have.
  */
-import '../images/apple-touch-icon-precomposed.png';
-import '../images/chrome-touch-icon-192x192-precomposed.png';
-import '../images/ms-touch-icon-144x144-precomposed.png';
+import '../images/apple-ninja152-precomposed.png';
+import '../images/chrome-ninja192-precomposed.png';
+import '../images/ms-ninja144-precomposed.png';
