b03031d8c85d16543901bfc6fa2939e59e31bd46
Simon Knox <s.c.knox@gmail.com>
2015-07-09 13:50:15 +1000

    absolute paths for assets

--- app/helmconfig.js
+++ app/helmconfig.js
@@ -21,7 +21,7 @@ const config = {
     // Add to homescreen for Safari on IOS 
     { "rel": "apple-touch-icon", "sizes": "152x152", applecon },
     { "rel": "stylesheet", "href": "https://fonts.googleapis.com/css?family=Roboto+Condensed", "type": "text/css" },
-    { "rel": "stylesheet", "href": "assets/styles/main.css" }
+    { "rel": "stylesheet", "href": "/assets/styles/main.css" }
     // SEO: If your mobile URL is different from the desktop URL, 
     // add a canonical link to the desktop page https://developers.google.com/webmasters/smartphone-sites/feature-phones
     // { "rel": "canonical", "href": "http://www.example.com/" }
