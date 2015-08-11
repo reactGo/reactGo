b700f23612c5689f18faf0a35677d1a22d679c6f
Ken <aenkil@gmail.com>
2015-08-11 22:15:51 +1000

    Hacky fix for requireAuth server side. Need a bit more thought about this.

--- app/utils/requireAuth.js
+++ app/utils/requireAuth.js
@@ -4,12 +4,16 @@ import UserWebAPIUtils from 'utils/UserWebAPIUtils';
 const requireAuth = (Component) => {
   return class Authenticated extends React.Component {
     static willTransitionTo(transition, params, query, callback) {
-      UserWebAPIUtils.loggedin().done((response) => {
-        if (!response.authenticated) {
-          transition.redirect('/login', {}, {'nextPath': transition.path});
-        }
+      if (typeof window !== 'undefined') {
+        UserWebAPIUtils.loggedin().done((response) => {
+          if (!response.authenticated) {
+            transition.redirect('/login', {}, {'nextPath': transition.path});
+          }
+          callback();
+        });
+      } else {
         callback();
-      });
+      }
     }
 
     render() {
