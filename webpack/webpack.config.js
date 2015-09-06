4f71ebd670bbfe6553284d95383107c566b747d9
Ken <aenkil@gmail.com>
2015-09-06 13:44:04 +1000

    Adding webpack uglifier

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -116,7 +116,8 @@ module.exports = [
     },
     plugins: [
         // extract inline css from modules into separate files
-        new ExtractTextPlugin("styles/main.css")
+        new ExtractTextPlugin("styles/main.css"),
+        new webpack.optimize.UglifyJsPlugin()
     ]
   }
 ];
