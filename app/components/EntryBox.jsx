fbaa314eed10c3cb189c0a4351cbf1eb20952ea2
Ken Ding <aenkil@gmail.com>
2015-11-19 07:36:59 +1100

    Making the syntax simpler for TopicTextInput

--- app/components/EntryBox.jsx
+++ app/components/EntryBox.jsx
@@ -5,25 +5,18 @@ import styles from 'scss/components/_entrybox';
 import TopicTextInput from 'components/TopicTextInput';
 
 class EntryBox extends React.Component {
-  /**
-   * Event handler called within TopicTextInput.
-   * Defining this here allows TopicTextInput to be used in multiple places
-   * in different ways.
-   * @param {string} text
-   */
-  _onSave = (text) => {
-    this.props.dispatch(createTopic(text));
-  }
-
-  _onChange = (text) => {
-    this.props.dispatch(typing(text));
-  }
 
   render() {
+    const { dispatch } = this.props;
     return (
       <div className={styles.entrybox}>
         <h1 className={styles.entrybox__header}>Vote for your top hack idea</h1>
-        <TopicTextInput className={styles.entrybox__input} value={this.props.topic} placeholder="What's yer fav idea?" onChange={this._onChange} onSave={this._onSave} />
+        <TopicTextInput 
+          className={styles.entrybox__input} 
+          value={this.props.topic} 
+          placeholder="What's yer fav idea?"
+          onChange={text => dispatch(typing(text))} 
+          onSave={text => dispatch(createTopic(text))} />
       </div>
     );
   }
