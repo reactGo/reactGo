a606c5cf130a12fed52b3a2e4f84054b64b2e88b
Ken Ding <aenkil@gmail.com>
2015-11-28 23:49:36 +1100

    Init for using redux-simple-router. Also cleaning up a lot of inconsistent code. We need to write tests for initialStates!

--- app/client.jsx
+++ app/client.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Iso from 'iso';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { ReduxRouter } from 'redux-router';
+import { Router, Route } from 'react-router';
+import { syncReduxAndRouter, routeReducer } from 'redux-simple-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import routes from 'routes.jsx';
+import rootReducer from 'reducers';
 import configureStore from 'store/configureStore';
 
-/*
- * Client side bootstrap with iso and redux
- */
-Iso.bootstrap((state, _, container) => {
-  let store = configureStore(state);
+// Grab the state from a global injected into 
+// server-generated HTML
+const initialState = window.__INITIAL_STATE__;
 
-  ReactDOM.render(
-    <Provider store={store}>
-      <ReduxRouter />
-		</Provider>, container);
+const store = configureStore(initialState);
+const history = createBrowserHistory();
 
-  if (process.env.NODE_ENV !== 'production') {
-  // Use require because imports can't be conditional.
-  // In production, you should ensure process.env.NODE_ENV
-  // is envified so that Uglify can eliminate this
-  // module and its dependencies as dead code.
-    require('./createDevToolsWindow')(store);
-  }
-});
+// Installs hooks that always keep react-router and redux
+// store in sync
+syncReduxAndRouter(history, store);
+
+render(
+  <Provider store={store}>
+    <Router history={history}>
+      {routes}
+    </Router>
+	</Provider>, document.getElementById('app'));
+
+if (process.env.NODE_ENV !== 'production') {
+// Use require because imports can't be conditional.
+// In production, you should ensure process.env.NODE_ENV
+// is envified so that Uglify can eliminate this
+// module and its dependencies as dead code.
+  require('./createDevToolsWindow')(store);
+}
--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { logOut } from 'actions/users';
 import styles from 'scss/components/_navigation';
 
-class Navigation extends React.Component {
+class Navigation extends Component {
 
   render() {
     const { dispatch } = this.props;
@@ -12,7 +12,7 @@ class Navigation extends React.Component {
       <nav className={styles.navigation} role="navigation">
           <Link to="/" className={styles.navigation__item + ' ' + styles['navigation__item--logo']} activeClassName={styles['navigation__item--active']}>Ninja Ocean</Link>
           { this.props.user.authenticated ? (
-            <Link onClick={()=> dispatch(logOut())} 
+            <Link onClick={()=> dispatch(logOut())}
               className={styles.navigation__item} to="/logout">Logout</Link>
           ) : (
             <Link className={styles.navigation__item} to="/login">Log in</Link>
@@ -25,7 +25,10 @@ class Navigation extends React.Component {
 
 }
 
-Navigation.propTypes = { user: React.PropTypes.object };
+Navigation.propTypes = {
+  user: PropTypes.object,
+  dispatch: PropTypes.func.isRequired
+};
 
 function mapStateToProps(state) {
   return {
