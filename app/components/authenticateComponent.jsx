0521a2331814c9842736b17c7438da4e7aa8a2a6
Ken Ding <aenkil@gmail.com>
2015-11-30 22:18:22 +1100

    reindent and make it work nicely with redux-simple-router's updatePath. Server side redirect still not fixed.

--- app/components/authenticateComponent.jsx
+++ app/components/authenticateComponent.jsx
@@ -1,38 +1,40 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { pushState } from 'redux-router';
+import { updatePath } from 'redux-simple-router';
 
 export function requireAuthentication(Component) {
-		class AuthenticateComponent extends React.Component {
+  class AuthenticateComponent extends React.Component {
 
-				constructor(props) {
-						super(props);
-				}
+    constructor(props) {
+      super(props);
+    }
 
-				componentWillMount() {
-						if (!this.props.isAuthenticated) {
-								this.props.dispatch(pushState(null, `/login?next=${this.props.location.pathname}`));
-						}
-				}
+    componentWillMount() {
+      const { isAuthenticated, dispatch } = this.props;
+      if (!isAuthenticated) {
+        dispatch(updatePath(`/login?next=${this.props.location.pathname}`));
+      }
+    }
 
-				render() {
-						return (
-							<Component {...this.props} />
-						);
-				}
-		}
+    render() {
+      return (
+        <Component {...this.props} />
+      );
+    }
+  }
 
-		AuthenticateComponent.propTypes = {
-			dispatch: React.PropTypes.func.isRequired,
-			isAuthenticated: React.PropTypes.bool,
-			location: React.PropTypes.object
-		};
+  AuthenticateComponent.propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+    location: PropTypes.object
+  };
 
-		const mapStateToProps =
-				(state) => ({
-						token: state.user.token,
-						isAuthenticated: state.user.authenticated
-				});
+  function mapStateToProps(state) {
+    return {
+      token: state.user.token,
+      isAuthenticated: state.user.authenticated
+    };
+  }
 
-		return connect(mapStateToProps)(AuthenticateComponent);
-}
+  return connect(mapStateToProps)(AuthenticateComponent);
+};
