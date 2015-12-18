a0c5399ba472b93d6543149398a275d13b66f442
Choon Ken Ding <aenkil@gmail.com>
2015-12-18 18:19:48 +1100

    Initial test file to test topic actions in redux

---
+++ test/actions/topics.test.js
@@ -0,0 +1,8 @@
+import expect from 'expect';
+import * as actions from '../../app/actions/topics';
+
+describe('Topic Actions', () => {
+  it('create CREATE_TOPIC action when a topic is entered', () => {
+
+  });
+});
