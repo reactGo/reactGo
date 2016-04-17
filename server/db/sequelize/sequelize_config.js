5c397771340040b81afd188a855ed3a63a148a77
Michael Jewell <michaeljewell9911@gmail.com>
2016-04-16 22:38:06 -0700

    add sequelize configs and model schemas

---
+++ server/db/sequelize/sequelize_config.js
@@ -0,0 +1,24 @@
+/** Important **/
+/** You should not be committing this file to GitHub **/
+/** Repeat: DO! NOT! COMMIT! THIS! FILE! TO! YOUR! REPO! **/
+
+module.exports = {
+  development: {
+    username: process.env.PGUSER || 'root',
+    password: null,
+    database: 'react_webpack_node_development',
+    host: '127.0.0.1',
+    // TODO: pull this based on db_type
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.PGUSER || 'root',
+    password: null,
+    database: 'react_webpack_node_test',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'POSTGRES_DB_URL'
+  }
+};
