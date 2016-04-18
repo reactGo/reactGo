cff400e031d19711e1037f0dd9e4bedf020279c7
Michael Jewell <michaeljewell9911@gmail.com>
2016-04-18 08:22:51 -0700

    use old require style so we can conditionally load database configs

--- server/db/index.js
+++ server/db/index.js
@@ -1,20 +1,20 @@
 import { DB_TYPE } from '../config/appConfig';
 import { DB_TYPES } from '../config/constants';
-import * as mongo from './mongo';
-import * as postgres from './postgres';
-import * as none from './none';
+// import * as mongo from './mongo';
+// import * as postgres from './postgres';
+// import * as none from './none';
 
 let dbConfig = null;
 
 switch (DB_TYPE) {
   case DB_TYPES.MONGO:
-    dbConfig = mongo;
+    dbConfig = require('./mongo');
     break;
   case DB_TYPES.POSTGRES:
-    dbConfig = postgres;
+    dbConfig = require('./postgres');
     break;
   case DB_TYPES.NONE:
-    dbConfig = none;
+    dbConfig = require('./none');
     break;
   default:
     throw new Error(`No database type '${DB_TYPE}' found`);
