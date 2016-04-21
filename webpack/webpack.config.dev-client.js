d276e1ec6a53af0737ad5edac58f537e42fab825
Simon Alexander Hewat <s.a.hewat@gmail.com>
2016-04-21 07:42:45 +0000

    added es7 style decorators to babel via https://www.npmjs.com/package/babel-plugin-transform-decorators-legacy

--- webpack/webpack.config.dev-client.js
+++ webpack/webpack.config.dev-client.js
@@ -15,6 +15,7 @@ var commonLoaders = [
     // https://github.com/gaearon/react-transform-hmr/issues/5#issuecomment-142313637
     query: {
       "presets": ["react-hmre", "es2015", "react", "stage-0"]
+      "plugins": ["transform-decorators-legacy"]
     },
     include: path.join(__dirname, '..', 'app'),
     exclude: path.join(__dirname, '..', 'node_modules')
--- webpack/webpack.config.dev-server.js
+++ webpack/webpack.config.dev-server.js
@@ -13,7 +13,8 @@ var commonLoaders = [
     // Reason why we put this here instead of babelrc
     // https://github.com/gaearon/react-transform-hmr/issues/5#issuecomment-142313637
     query: {
-      "presets": ["es2015", "react", "stage-0"]
+      "presets": ["es2015", "react", "stage-0"],
+      "plugins": ["transform-decorators-legacy"]
     },
     include: path.join(__dirname, '..', 'app'),
     exclude: path.join(__dirname, '..', 'node_modules')
