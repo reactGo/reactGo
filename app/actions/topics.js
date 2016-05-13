reactGo/reactGo

commit eaca683f6ddc216d25da5955f333b2091ab0c4cc

Added redux-axios-middleware & refactor of topic requests

app/actions/topics.js
@@ -1,38 +1,5 @@
 /* eslint consistent-return: 0, no-else-return: 0*/
-import { polyfill } from 'es6-promise';
-import request from 'axios';
-import md5 from 'spark-md5';
 import * as types from 'types';
-
-polyfill();
-
-/*
- * Utility function to make AJAX requests using isomorphic fetch.
- * You can also use jquery's $.ajax({}) if you do not want to use the
- * /fetch API.
- * Note: this function relies on an external variable `API_ENDPOINT`
- *        and isn't a pure function
- * @param Object Data you wish to pass to the server
- * @param String HTTP method, e.g. post, get, put, delete
- * @param String endpoint
- * @return Promise
- */
-function makeTopicRequest(method, id, data, api = '/topic') {
-  return request[method](api + (id ? ('/' + id) : ''), data);
-}
-
-function increment(index) {
-  return { type: types.INCREMENT_COUNT, index };
-}
-
-function decrement(index) {
-  return { type: types.DECREMENT_COUNT, index };
-}
-
-function destroy(index) {
-  return { type: types.DESTROY_TOPIC, index };
-}
-
 
 export function typing(text) {
   return {
@@ -41,131 +8,81 @@ export function typing(text) {
   };
 }
 
-/*
- * @param data
- * @return a simple JS object
- */
-function createTopicRequest(data) {
-  return {
-    type: types.CREATE_TOPIC_REQUEST,
-    id: data.id,
-    count: data.count,
-    text: data.text
-  };
-}
-
-function createTopicSuccess() {
-  return {
-    type: types.CREATE_TOPIC_SUCCESS
-  };
-}
-
-function createTopicFailure(data) {
-  return {
-    type: types.CREATE_TOPIC_FAILURE,
-    id: data.id,
-    error: data.error
-  };
-}
-
-function createTopicDuplicate() {
-  return {
-    type: types.CREATE_TOPIC_DUPLICATE
-  };
-}
-
-// This action creator returns a function,
-// which will get executed by Redux-Thunk middleware
-// This function does not need to be pure, and thus allowed
-// to have side effects, including executing asynchronous API calls.
 export function createTopic(text) {
-  return (dispatch, getState) => {
-    // If the text box is empty
-    if (text.trim().length <= 0) return;
+  // If the text box is empty
+  if (text.trim().length <= 0) return;
 
-    const id = md5.hash(text);
-    // Redux thunk's middleware receives the store methods `dispatch`
-    // and `getState` as parameters
-    const { topic } = getState();
-    const data = {
+  const request = {
+    method: 'post',
+    url: '/topic',
+    data: {
       count: 1,
-      id,
       text
-    };
-
-    // Conditional dispatch
-    // If the topic already exists, make sure we emit a dispatch event
-    if (topic.topics.filter(topicItem => topicItem.id === id).length > 0) {
-      // Currently there is no reducer that changes state for this
-      // For production you would ideally have a message reducer that
-      // notifies the user of a duplicate topic
-      return dispatch(createTopicDuplicate());
     }
-
-    // First dispatch an optimistic update
-    dispatch(createTopicRequest(data));
-
-    return makeTopicRequest('post', id, data)
-      .then(res => {
-        if (res.status === 200) {
-          // We can actually dispatch a CREATE_TOPIC_SUCCESS
-          // on success, but I've opted to leave that out
-          // since we already did an optimistic update
-          // We could return res.json();
-          return dispatch(createTopicSuccess());
-        }
-      })
-      .catch(() => {
-        return dispatch(createTopicFailure({ id, error: 'Oops! Something went wrong and we couldn\'t create your topic'}));
-      });
   };
+
+  return {
+    types: [types.CREATE_TOPIC_REQUEST, types.CREATE_TOPIC_SUCCESS, types.CREATE_TOPIC_FAILURE],
+    payload: { request }
+  }
 }
 
 // Fetch posts logic
 export function fetchTopics() {
+  const request = {
+    method: 'get',
+    url: '/topic'
+  };
+
   return {
     type: types.GET_TOPICS,
-    promise: makeTopicRequest('get')
-  };
+    payload: { request }
+  }
 }
 
 
 export function incrementCount(id, index) {
-  return dispatch => {
-    dispatch(increment(index));
+  const request = {
+    method: 'put',
+    url: `/topic/${id}`,
+    data: {
+      isFull: false,
+      isIncrement: true
+    }
+  };
 
-    return makeTopicRequest('put', id, {
-        isFull: false,
-        isIncrement: true
-      });
-    // do something with the ajax response
-    // You can also dispatch here
-    // E.g.
-    // .then(response => {});
+  return {
+    types: [types.INCREMENT_COUNT_REQUEST, types.INCREMENT_COUNT_SUCCESS, types.INCREMENT_COUNT_FAILURE],
+    payload: { request },
+    index: index
   };
 }
 
 export function decrementCount(id, index) {
-  return dispatch => {
-    dispatch(decrement(index));
-    return makeTopicRequest('put', id, {
-        isFull: false,
-        isIncrement: false
-      });
-    // do something with the ajax response
-    // You can also dispatch here
-    // E.g.
-    // .then(response => {});
+  const request = {
+    method: 'put',
+    url: `/topic/${id}`,
+    data: {
+      isFull: false,
+      isIncrement: false
+    }
+  };
+
+  return {
+    types: [types.DECREMENT_COUNT_REQUEST, types.DECREMENT_COUNT_SUCCESS, types.DECREMENT_COUNT_FAILURE],
+    payload: { request },
+    index: index
   };
 }
 
 export function destroyTopic(id, index) {
-  return dispatch => {
-    dispatch(destroy(index));
-    return makeTopicRequest('delete', id);
-    // do something with the ajax response
-    // You can also dispatch here
-    // E.g.
-    // .then(response => {});
+  return {
+    types: [types.DESTROY_TOPIC_REQUEST, types.DESTROY_TOPIC_SUCCESS, types.DESTROY_TOPIC_FAILURE],
+    payload: {
+      request: {
+        method: 'delete',
+        url: `/topic/${id}`
+      }
+    }
   };
 }

app/components/MainSection.jsx
@@ -9,9 +9,8 @@ const MainSection = ({onIncrement, onDecrement, onDestroy, topics}) => {
   const topicItems = topics.map((topic, key) => {
     return (
       <TopicItem index={key}
-        id={topic.id}
         key={key}
-        text={topic.text}
+        topic={topic}
         onIncrement={onIncrement}
         onDecrement={onDecrement}
         onDestroy={onDestroy} />);

app/reducers/topic.js
@@ -1,10 +1,18 @@
 import {
   TYPING,
+  GET_TOPICS,
   CREATE_TOPIC_REQUEST,
+  CREATE_TOPIC_SUCCESS,
   CREATE_TOPIC_FAILURE,
-  DESTROY_TOPIC,
-  INCREMENT_COUNT,
-  DECREMENT_COUNT,
+  DESTROY_TOPIC_REQUEST,
+  DESTROY_TOPIC_SUCCESS,
+  DESTROY_TOPIC_FAILURE,
+  INCREMENT_COUNT_REQUEST,
+  INCREMENT_COUNT_SUCCESS,
+  INCREMENT_COUNT_FAILURE,
+  DECREMENT_COUNT_REQUEST,
+  DECREMENT_COUNT_SUCCESS,
+  DECREMENT_COUNT_FAILURE,
   GET_TOPICS_REQUEST,
   GET_TOPICS_SUCCESS,
   GET_TOPICS_FAILURE } from 'types';
@@ -19,6 +27,10 @@ export default function topic(state = {
       return Object.assign({}, state,
         { newTopic: action.newTopic }
       );
+    case GET_TOPICS:
+      return Object.assign({}, state,
+        { topics: action.payload }
+      );
     case GET_TOPICS_REQUEST:
       return Object.assign({}, state, {
         isFetching: true
@@ -26,49 +38,86 @@ export default function topic(state = {
     case GET_TOPICS_SUCCESS:
       return Object.assign({}, state, {
         isFetching: false,
-        topics: action.req.data
+        topics: action.payload
       });
     case GET_TOPICS_FAILURE:
       return Object.assign({}, state, {
         isFetching: false
       });
     case CREATE_TOPIC_REQUEST:
-      return {
-        topics: [...state.topics, { id: action.id, count: action.count, text: action.text }],
+      return Object.assign({}, state, {
+        isFetching: true
+      });
+    case CREATE_TOPIC_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        topics: [...state.topics, action.payload],
         newTopic: ''
-      };
+      });
     case CREATE_TOPIC_FAILURE:
       return {
-        topics: [...state.topics.filter((tp) => tp.id !== action.id)],
+        isFetching: false,
+        topics: state.topics,
         newTopic: state.newTopic
       };
-    case DESTROY_TOPIC:
+    case DESTROY_TOPIC_REQUEST:
       return {
-        topics: [...state.topics.filter((tp, i) => i !== action.index)],
+        isFetching: true,
+        topics: state.topics,
         newTopic: state.newTopic
       };
-    case INCREMENT_COUNT:
+    case DESTROY_TOPIC_SUCCESS:
       return {
+        isFetching: false,
+        topics: state.topics.filter((obj) => obj._id !== action.payload._id),
+        newTopic: state.newTopic
+      };
+    case DESTROY_TOPIC_FAILURE:
+      return {
+        isFetching: false,
+        topics: state.topics,
+        newTopic: state.newTopic
+      };
+    case INCREMENT_COUNT_REQUEST:
+      return Object.assign({}, state, {
+        isFetching: true
+      });
+    case INCREMENT_COUNT_SUCCESS:
+      return {
+        isFetching: false,
         topics: [
-        ...state.topics.slice(0, action.index),
-        Object.assign({}, state.topics[action.index], {
-          count: state.topics[action.index].count + 1
-        }),
-        ...state.topics.slice(action.index + 1)
+          ...state.topics.slice(0, action.index),
+          Object.assign({}, state.topics[action.index], {
+            count: action.payload.count
+          }),
+          ...state.topics.slice(action.index + 1)
         ],
         newTopic: state.newTopic
       };
-    case DECREMENT_COUNT:
+    case INCREMENT_COUNT_FAILURE:
+      return Object.assign({}, state, {
+        isFetching: false
+      });
+    case DECREMENT_COUNT_REQUEST:
+      return Object.assign({}, state, {
+        isFetching: true
+      });
+    case DECREMENT_COUNT_SUCCESS:
       return {
+        isFetching: false,
         topics: [
-        ...state.topics.slice(0, action.index),
-        Object.assign({}, state.topics[action.index], {
-          count: state.topics[action.index].count - 1
-        }),
-        ...state.topics.slice(action.index + 1)
+          ...state.topics.slice(0, action.index),
+          Object.assign({}, state.topics[action.index], {
+            count: action.payload.count
+          }),
+          ...state.topics.slice(action.index + 1)
         ],
         newTopic: state.newTopic
       };
+    case DECREMENT_COUNT_FAILURE:
+      return Object.assign({}, state, {
+        isFetching: false
+      });
 
     default:
       return state;

server/db/mongo/controllers/topics.js
@@ -8,7 +8,7 @@ export function all(req, res) {
   Topic.find({}).exec((err, topics) => {
     if (err) {
       console.log('Error in first query');
-      return res.status(500).send('Something went wrong getting the data');
+      return res.sendStatus(500);
     }
 
     return res.json(topics);
@@ -19,13 +19,13 @@ export function all(req, res) {
  * Add a Topic
  */
 export function add(req, res) {
-  Topic.create(req.body, (err) => {
+  Topic.create(req.body, (err, data) => {
     if (err) {
       console.log(err);
       return res.status(400).send(err);
     }
 
-    return res.status(200).send('OK');
+    return res.send(data);
   });
 }
 
@@ -33,30 +33,23 @@ export function add(req, res) {
  * Update a topic
  */
 export function update(req, res) {
-  const query = { id: req.params.id };
   const isIncrement = req.body.isIncrement;
   const isFull = req.body.isFull;
-  const omitKeys = ['id', '_id', '_v', 'isIncrement', 'isFull'];
+  const omitKeys = ['_id', '_v', 'isIncrement', 'isFull'];
   const data = _.omit(req.body, omitKeys);
+  const callback = (err, updated) => {
+    if (err) {
+      console.log('Error on save!');
+      return res.sendStatus(500);
+    }
+
+    return res.send(updated);
+  };
 
   if (isFull) {
-    Topic.findOneAndUpdate(query, data, (err) => {
-      if (err) {
-        console.log('Error on save!');
-        return res.status(500).send('We failed to save for some reason');
-      }
-
-      return res.status(200).send('Updated successfully');
-    });
+    Topic.findOneAndUpdate({'_id': req.params.id}, data, callback);
   } else {
-    Topic.findOneAndUpdate(query, { $inc: { count: isIncrement ? 1 : -1 } }, (err) => {
-      if (err) {
-        console.log('Error on save!');
-        return res.status(500).send('We failed to save for some reason');
-      }
-
-      return res.status(200).send('Updated successfully');
-    });
+    Topic.findOneAndUpdate({'_id': req.params.id}, { $inc: { count: isIncrement ? 1 : -1 } }, callback);
   }
 }
 
@@ -64,14 +57,17 @@ export function update(req, res) {
  * Remove a topic
  */
 export function remove(req, res) {
-  const query = { id: req.params.id };
-  Topic.findOneAndRemove(query, (err) => {
+  Topic.findOneAndRemove({'_id': req.params.id}, (err, deleted) => {
     if (err) {
       console.log('Error on delete');
-      return res.status(500).send('We failed to delete for some reason');
+      return res.sendStatus(500);
     }
 
-    return res.status(200).send('Removed Successfully');
+    if (!deleted) {
+      return res.sendStatus(404);
+    }
+
+    return res.send(deleted);
   });
 }
 

app/api/promiseMiddleware.js
@@ -1,26 +1,0 @@
-/*
-* Redux middleware to handle promises
-* As seen in: https://github.com/caljrimmer/isomorphic-redux-app
-*/
-
-export default function promiseMiddleware() {
-  return next => action => {
-    const { promise, type, ...rest } = action;
-
-    if (!promise) return next(action);
-
-    const SUCCESS = type + '_SUCCESS';
-    const REQUEST = type + '_REQUEST';
-    const FAILURE = type + '_FAILURE';
-    next({ ...rest, type: REQUEST });
-    return promise
-      .then(req => {
-        next({ ...rest, req, type: SUCCESS });
-        return true;
-      })
-      .catch(error => {
-        next({ ...rest, error, type: FAILURE });
-        return false;
-      });
-   };
-}

app/components/TopicItem.jsx
@@ -13,24 +13,24 @@ export default class TopicItem extends Component {
   }
 
   onIncrement() {
-    const { id, index, onIncrement } = this.props;
-    onIncrement(id, index);
+    const { topic, index, onIncrement } = this.props;
+    onIncrement(topic._id, index);
   }
 
   onDecrement() {
-    const { id, index, onDecrement } = this.props;
-    onDecrement(id, index);
+    const { topic, index, onDecrement } = this.props;
+    onDecrement(topic._id, index);
   }
 
   onDestroyClick() {
-    const { id, index, onDestroy } = this.props;
-    onDestroy(id, index);
+    const { topic, onDestroy } = this.props;
+    onDestroy(topic._id);
   }
 
   render() {
     return (
-      <li className={cx('topic-item')} key={this.props.id}>
-        <span className={cx('topic')}>{this.props.text}</span>
+      <li className={cx('topic-item')}>
+        <span className={cx('topic')}>{this.props.topic.text}</span>
         <button className={
           cx('button', 'increment')
         } onClick={this.onIncrement}>+</button>
@@ -46,9 +46,8 @@ export default class TopicItem extends Component {
 }
 
 TopicItem.propTypes = {
-  text: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
   index: PropTypes.number.isRequired,
+  topic: PropTypes.object.isRequired,
   onIncrement: PropTypes.func.isRequired,
   onDecrement: PropTypes.func.isRequired,
   onDestroy: PropTypes.func.isRequired

app/containers/Vote.jsx
@@ -40,9 +40,9 @@ class Vote extends Component {
     dispatch(decrementCount(id, index));
   }
 
-  onDestroy(id, index) {
+  onDestroy(id) {
     const { dispatch } = this.props;
-    dispatch(destroyTopic(id, index));
+    dispatch(destroyTopic(id));
   }
 
   onEntryChange(text) {

app/server.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { RouterContext, match, createMemoryHistory } from 'react-router'
-import axios from 'axios';
 import { Provider } from 'react-redux';
 import createRoutes from 'routes';
 import configureStore from 'store/configureStore';
 import headconfig from 'components/Meta';
 import { fetchComponentDataBeforeRender } from 'api/fetchComponentDataBeforeRender';
-
-const clientConfig = {
-  host: process.env.HOSTNAME || 'localhost',
-  port: process.env.PORT || '3000'
-};
-
-// configure baseURL for axios requests (for serverside API calls)
-axios.defaults.baseURL = `http://${clientConfig.host}:${clientConfig.port}`;
 
 /*
  * Our html template file

commit 1f0fcef769fe54844ddc4c7d07162fcbb20d2680

Fixed a bug which caused by eaca683

app/actions/topics.js
@@ -35,7 +35,7 @@ export function fetchTopics() {
   };
 
   return {
-    type: types.GET_TOPICS,
+    types: [types.GET_TOPICS_REQUEST, types.GET_TOPICS_SUCCESS, types.GET_TOPICS_FAILURE],
     payload: { request }
   }
 }
@@ -75,7 +75,7 @@ export function decrementCount(id, index) {
   };
 }
 
-export function destroyTopic(id, index) {
+export function destroyTopic(id) {
   return {
     types: [types.DESTROY_TOPIC_REQUEST, types.DESTROY_TOPIC_SUCCESS, types.DESTROY_TOPIC_FAILURE],
     payload: {

app/components/MainSection.jsx
@@ -8,8 +8,8 @@ const cx = classNames.bind(styles);
 const MainSection = ({onIncrement, onDecrement, onDestroy, topics}) => {
   const topicItems = topics.map((topic, key) => {
     return (
-      <TopicItem index={key}
-        key={key}
+      <TopicItem key={key}
+        index={key}
         topic={topic}
         onIncrement={onIncrement}
         onDecrement={onDecrement}

app/reducers/topic.js
@@ -1,6 +1,5 @@
 import {
   TYPING,
-  GET_TOPICS,
   CREATE_TOPIC_REQUEST,
   CREATE_TOPIC_SUCCESS,
   CREATE_TOPIC_FAILURE,
@@ -26,10 +25,6 @@ export default function topic(state = {
     case TYPING:
       return Object.assign({}, state,
         { newTopic: action.newTopic }
-      );
-    case GET_TOPICS:
-      return Object.assign({}, state,
-        { topics: action.payload }
       );
     case GET_TOPICS_REQUEST:
       return Object.assign({}, state, {

server/db/mongo/controllers/topics.js
@@ -33,13 +33,12 @@ export function add(req, res) {
  * Update a topic
  */
 export function update(req, res) {
-  const isIncrement = req.body.isIncrement;
-  const isFull = req.body.isFull;
+  const { isIncrement, isFull } = req.body;
   const omitKeys = ['_id', '_v', 'isIncrement', 'isFull'];
   const data = _.omit(req.body, omitKeys);
   const callback = (err, updated) => {
     if (err) {
-      console.log('Error on save!');
+      console.error(err);
       return res.sendStatus(500);
     }
 
@@ -47,9 +46,9 @@ export function update(req, res) {
   };
 
   if (isFull) {
-    Topic.findOneAndUpdate({'_id': req.params.id}, data, callback);
+    Topic.findOneAndUpdate({'_id': req.params.id}, data, {'new': true}, callback);
   } else {
-    Topic.findOneAndUpdate({'_id': req.params.id}, { $inc: { count: isIncrement ? 1 : -1 } }, callback);
+    Topic.findOneAndUpdate({'_id': req.params.id}, { '$inc': { count: isIncrement ? 1 : -1 } }, {'new': true}, callback);
   }
 }
 