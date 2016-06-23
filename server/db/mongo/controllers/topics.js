d49278c039aa6643bc382719a37369dc9b55016d
Ken Ding <aenkil@gmail.com>
2016-06-24 07:35:03 +1000

    Revert "[SPLIT #221]  Updated backend code for new components (#270)"
    This reverts commit 6058f01de62309c12aeee7ef89e7942c0ffaf57e.

--- server/db/mongo/controllers/topics.js
+++ server/db/mongo/controllers/topics.js
@@ -8,7 +8,7 @@ export function all(req, res) {
   Topic.find({}).exec((err, topics) => {
     if (err) {
       console.log('Error in first query');
-      return res.sendStatus(500);
+      return res.status(500).send('Something went wrong getting the data');
     }
 
     return res.json(topics);
@@ -19,13 +19,13 @@ export function all(req, res) {
  * Add a Topic
  */
 export function add(req, res) {
-  Topic.create(req.body, (err, data) => {
+  Topic.create(req.body, (err) => {
     if (err) {
       console.log(err);
       return res.status(400).send(err);
     }
 
-    return res.status(data);
+    return res.status(200).send('OK');
   });
 }
 
@@ -33,22 +33,30 @@ export function add(req, res) {
  * Update a topic
  */
 export function update(req, res) {
-  const { isIncrement, isFull } = req.body;
-  const omitKeys = ['_id', '_v', 'isIncrement', 'isFull'];
+  const query = { id: req.params.id };
+  const isIncrement = req.body.isIncrement;
+  const isFull = req.body.isFull;
+  const omitKeys = ['id', '_id', '_v', 'isIncrement', 'isFull'];
   const data = _.omit(req.body, omitKeys);
-  const callback = (err, updated) => {
-    if (err) {
-      console.error(err);
-      return res.sendStatus(500);
-    }
-
-    return res.send(updated);
-  }
 
   if (isFull) {
-    Topic.findOneAndUpdate({'_id': req.params.id}, data, {'new': true}, callback);
+    Topic.findOneAndUpdate(query, data, (err) => {
+      if (err) {
+        console.log('Error on save!');
+        return res.status(500).send('We failed to save for some reason');
+      }
+
+      return res.status(200).send('Updated successfully');
+    });
   } else {
-    Topic.findOneAndUpdate({'_id': req.params.id}, { '$inc': { count: isIncrement ? 1 : -1 } }, {'new': true}, callback);
+    Topic.findOneAndUpdate(query, { $inc: { count: isIncrement ? 1 : -1 } }, (err) => {
+      if (err) {
+        console.log('Error on save!');
+        return res.status(500).send('We failed to save for some reason');
+      }
+
+      return res.status(200).send('Updated successfully');
+    });
   }
 }
 
@@ -57,17 +65,13 @@ export function update(req, res) {
  */
 export function remove(req, res) {
   const query = { id: req.params.id };
-  Topic.findOneAndRemove({'_id': req.params.id}, (err, deleted) => {
+  Topic.findOneAndRemove(query, (err) => {
     if (err) {
       console.log('Error on delete');
-      return res.sendStatus(500);
+      return res.status(500).send('We failed to delete for some reason');
     }
 
-    if (!deleted) {
-      return res.sendStatus(404);
-    }
-
-    return res.send(deleted);
+    return res.status(200).send('Removed Successfully');
   });
 }
 
