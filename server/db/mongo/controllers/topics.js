6058f01de62309c12aeee7ef89e7942c0ffaf57e
18601673727 <18601673727@163.com>
2016-06-24 05:22:54 +0800

    [SPLIT #221]  Updated backend code for new components (#270)

--- server/db/mongo/controllers/topics.js
+++ server/db/mongo/controllers/topics.js
@@ -8,7 +8,7 @@ export function all(req, res) {
   Topic.find({}).exec((err, topics) => {
     if (err) {
       console.log('Error in first query');
-      return res.status(500).send('Something went wrong getting the data');
+      return res.sendStatus(500);
     }
 
     return res.json(topics);
@@ -19,13 +19,13 @@ export function all(req, res) {
  * Add a Topic
  */
 export function add(req, res) {
-  Topic.create(req.body, (err) => {
+  Topic.create(req.body, (err, data) => {
     if (err) {
       console.log(err);
       return res.status(400).send(err);
     }
 
-    return res.status(200).send('OK');
+    return res.status(data);
   });
 }
 
@@ -33,30 +33,22 @@ export function add(req, res) {
  * Update a topic
  */
 export function update(req, res) {
-  const query = { id: req.params.id };
-  const isIncrement = req.body.isIncrement;
-  const isFull = req.body.isFull;
-  const omitKeys = ['id', '_id', '_v', 'isIncrement', 'isFull'];
+  const { isIncrement, isFull } = req.body;
+  const omitKeys = ['_id', '_v', 'isIncrement', 'isFull'];
   const data = _.omit(req.body, omitKeys);
+  const callback = (err, updated) => {
+    if (err) {
+      console.error(err);
+      return res.sendStatus(500);
+    }
+
+    return res.send(updated);
+  }
 
   if (isFull) {
-    Topic.findOneAndUpdate(query, data, (err) => {
-      if (err) {
-        console.log('Error on save!');
-        return res.status(500).send('We failed to save for some reason');
-      }
-
-      return res.status(200).send('Updated successfully');
-    });
+    Topic.findOneAndUpdate({'_id': req.params.id}, data, {'new': true}, callback);
   } else {
-    Topic.findOneAndUpdate(query, { $inc: { count: isIncrement ? 1 : -1 } }, (err) => {
-      if (err) {
-        console.log('Error on save!');
-        return res.status(500).send('We failed to save for some reason');
-      }
-
-      return res.status(200).send('Updated successfully');
-    });
+    Topic.findOneAndUpdate({'_id': req.params.id}, { '$inc': { count: isIncrement ? 1 : -1 } }, {'new': true}, callback);
   }
 }
 
@@ -65,13 +57,17 @@ export function update(req, res) {
  */
 export function remove(req, res) {
   const query = { id: req.params.id };
-  Topic.findOneAndRemove(query, (err) => {
+  Topic.findOneAndRemove({'_id': req.params.id}, (err, deleted) => {
     if (err) {
       console.log('Error on delete');
-      return res.status(500).send('We failed to delete for some reason');
+      return res.sendStatus(500);
     }
 
-    return res.status(200).send('Removed Successfully');
+    if (!deleted) {
+      return res.sendStatus(404);
+    }
+
+    return res.send(deleted);
   });
 }
 
