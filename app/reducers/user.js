59aa90754141aeba7914298615eadb217cb91ae5
Ken Ding <aenkil@gmail.com>
2016-07-06 20:05:36 +1000

    Refactor user reducer to compose smaller reducers

--- app/reducers/user.js
+++ app/reducers/user.js
@@ -1,28 +1,83 @@
 import * as types from 'types';
+import { combineReducers } from 'redux';
 
-export default function user(state = {
-  isLogin: true,
-  message: '',
-  isWaiting: false,
-  authenticated: false }, action = {}) {
-  switch (action.type) {
+const isLogin = (
+  state = true,
+  action
+) => {
+  switch(action.type) {
     case types.TOGGLE_LOGIN_MODE:
-      return {...state, isLogin: !state.isLogin, message: ''};
-    case types.MANUAL_LOGIN_USER:
-    case types.SIGNUP_USER:
-    case types.LOGOUT_USER:
-      return {...state, isWaiting: true, message: ''};
-    case types.LOGIN_SUCCESS_USER:
-    case types.SIGNUP_SUCCESS_USER:
-      return {...state, isWaiting: false, authenticated: true, message: ''};
-    case types.LOGIN_ERROR_USER:
-    case types.SIGNUP_ERROR_USER:
-      return {...state, isWaiting: false, authenticated: false, message: action.message};
-    case types.LOGOUT_SUCCESS_USER:
-      return {...state, isWaiting: false, authenticated: false, message: ''};
-    case types.LOGOUT_ERROR_USER:
-      return {...state, isWaiting: false, authenticated: true, isLogin: true};
+      return !state;
     default:
       return state;
   }
-}
+};
+
+const message = (
+  state = '',
+  action
+) => {
+  switch (action.type) {
+    case types.TOGGLE_LOGIN_MODE:
+    case types.MANUAL_LOGIN_USER:
+    case types.SIGNUP_USER:
+    case types.LOGOUT_USER:
+    case types.LOGIN_SUCCESS_USER:
+    case types.SIGNUP_SUCCESS_USER:
+      return '';
+    case types.LOGIN_ERROR_USER:
+    case types.SIGNUP_ERROR_USER:
+      return action.message;
+    default:
+      return state;
+  }
+};
+
+const isWaiting = (
+  state = false,
+  action
+) => {
+  switch (action.type) {
+    case types.MANUAL_LOGIN_USER:
+    case types.SIGNUP_USER:
+    case types.LOGOUT_USER:
+      return true;
+    case types.LOGIN_SUCCESS_USER:
+    case types.SIGNUP_SUCCESS_USER:
+    case types.LOGOUT_SUCCESS_USER:
+    case types.LOGIN_ERROR_USER:
+    case types.SIGNUP_ERROR_USER:
+    case types.LOGOUT_ERROR_USER:
+      return false;
+    default:
+      return state;
+  }
+};
+
+const authenticated = (
+  state = false,
+  action
+) => {
+  switch (action.type) {
+    case types.LOGIN_SUCCESS_USER:
+    case types.SIGNUP_SUCCESS_USER:
+    case types.LOGOUT_ERROR_USER:
+      return true;
+    case types.LOGIN_ERROR_USER:
+    case types.SIGNUP_ERROR_USER:
+    case types.LOGOUT_SUCCESS_USER:
+      return false;
+    default:
+      return state;
+  }
+};
+
+const userReducer = combineReducers({
+  isLogin,
+  isWaiting,
+  authenticated,
+  message
+});
+
+export default userReducer;
+
