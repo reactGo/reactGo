b95bd0983c2a842a6824b395de3d9ae9d2a1a33e
John Rodler <j@jrod.io>
2016-08-01 04:26:19 -0700

    Enzyme Support (React Component Testing) (#384)
    * switch to enzyme react-component unit-testing
    
    * cleanup MainSection + TopicItem components

--- app/components/TopicItem.jsx
+++ app/components/TopicItem.jsx
@@ -1,54 +1,42 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import classNames from 'classnames/bind';
 import styles from 'css/components/topic-item';
 
 const cx = classNames.bind(styles);
 
-export default class TopicItem extends Component {
-  constructor(props) {
-    super(props);
-    this.onIncrement = this.onIncrement.bind(this);
-    this.onDecrement = this.onDecrement.bind(this);
-    this.onDestroyClick = this.onDestroyClick.bind(this);
-  }
+const TopicItem = ({ text, id, incrementCount, decrementCount, destroyTopic }) => {
+  const onIncrement = () => {
+    incrementCount(id);
+  };
+  const onDecrement = () => {
+    decrementCount(id);
+  };
+  const onDestroy = () => {
+    destroyTopic(id);
+  };
 
-  onIncrement() {
-    const { id, onIncrement } = this.props;
-    onIncrement(id);
-  }
-
-  onDecrement() {
-    const { id, onDecrement } = this.props;
-    onDecrement(id);
-  }
-
-  onDestroyClick() {
-    const { id, onDestroy } = this.props;
-    onDestroy(id) ;
-  }
-
-  render() {
-    return (
-      <li className={cx('topic-item')} key={this.props.id}>
-        <span className={cx('topic')}>{this.props.text}</span>
-        <button className={
-          cx('button', 'increment')
-        } onClick={this.onIncrement}>+</button>
-        <button className={
-          cx('button', 'decrement')
-        } onClick={this.onDecrement}>-</button>
-        <button className={
-          cx('button', 'destroy')
-        } onClick={this.onDestroyClick}>{String.fromCharCode(215)}</button>
-      </li>
-    );
-  }
-}
+  return (
+    <li className={cx('topic-item')} key={id}>
+      <span className={cx('topic')}>{text}</span>
+      <button
+        className={cx('button', 'increment')}
+        onClick={onIncrement}>+</button>
+      <button
+        className={cx('button', 'decrement')}
+        onClick={onDecrement}>-</button>
+      <button
+        className={cx('button', 'destroy')}
+        onClick={onDestroy}>{String.fromCharCode(215)}</button>
+    </li>
+  );
+};
 
 TopicItem.propTypes = {
   text: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
-  onIncrement: PropTypes.func.isRequired,
-  onDecrement: PropTypes.func.isRequired,
-  onDestroy: PropTypes.func.isRequired
+  incrementCount: PropTypes.func.isRequired,
+  decrementCount: PropTypes.func.isRequired,
+  destroyTopic: PropTypes.func.isRequired
 };
+
+export default TopicItem;
