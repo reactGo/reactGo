784c20d99f3bffeb591f9ffb11b0f98e0b20dcaa
shredd <bigair.snowboard@icloud.com>
2016-11-01 14:15:42 -0400

    Revert so that I create a pull request in the main project "Fix for unauthenticated user gets authenticated #546"
    This reverts commit 517192610f2f28c2bb4c6b62fc916ec57f50d077.

--- server/db/sequelize/passport/local.js
+++ server/db/sequelize/passport/local.js
@@ -6,10 +6,9 @@ export default (email, password, done) =>
   User.findOne({ where: { email } }).then((user) => {
     if (!user) return done(null, false, { message: `There is no record of the email ${email}.` });
     return user.comparePassword(password).then(
-      (result) => {
-        if(result) done(null, user);
-        else done(null, false, { message: 'Your email/password combination is incorrect.' });
-      });
+      () => done(null, user),
+      () => done(null, false, { message: 'Your email/password combination is incorrect.' })
+    );
   }).catch((err) => {
     console.log(err);
     done(null, false, { message: 'Something went wrong trying to authenticate' });
