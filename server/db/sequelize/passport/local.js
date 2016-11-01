8d367b2d4f3e1b19bd44f89a0cb6b2c859a0266f
shredd <bigair.snowboard@icloud.com>
2016-11-01 14:22:47 -0400

    Fix the issue: unauthenticated user gets authenticated #546

--- server/db/sequelize/passport/local.js
+++ server/db/sequelize/passport/local.js
@@ -6,9 +6,10 @@ export default (email, password, done) =>
   User.findOne({ where: { email } }).then((user) => {
     if (!user) return done(null, false, { message: `There is no record of the email ${email}.` });
     return user.comparePassword(password).then(
-      () => done(null, user),
-      () => done(null, false, { message: 'Your email/password combination is incorrect.' })
-    );
+      (result) => {
+        if(result) done(null, user);
+        else done(null, false, { message: 'Your email/password combination is incorrect.' });
+    });
   }).catch((err) => {
     console.log(err);
     done(null, false, { message: 'Something went wrong trying to authenticate' });
