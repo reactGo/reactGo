af3f15ab09328e6533ba1a7f7fec3f869bb44990
StefanWerW <scaw.dev@gmail.com>
2016-11-03 22:15:11 +0100

    more readable

--- app/middlewares/preRenderMiddleware.js
+++ app/middlewares/preRenderMiddleware.js
@@ -3,6 +3,14 @@ import {flatten} from 'lodash'
 //Dispatch is from the store
 //FetchData is either falsy (null, undefined etc), an action (function, Promise) or an array of either:
 export default(dispatch, { routes, params, location: { query } }) => {
-  const dataFetchers = flatten(routes.filter(({fetchData}) => !!fetchData).map(e=>e.fetchData))
-  return Promise.all(dataFetchers.map(fetchData => dispatch(fetchData({ ...params, query }))))
+  // get all routes (that has fetchData) as a flat array
+  const flatRoutes = flatten(routes.filter(({fetchData}) => !!fetchData)
+
+  // get each routes fetchData
+  const dataFetchers= flatRoutes.map(fr => fr.fetchData)
+
+  // flatten dataFetchers in case of arrays (<Route fetchData={[a, b, c]})/>)
+  const flatDataFetchers= flatten(dataFetchers)
+
+  return Promise.all(flatDataFetchers.map(dataFetcher => dispatch(dataFetcher({ ...params, query }))))
 }
