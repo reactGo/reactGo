d3f7e61ab9b0e25bbf8a59683f11c05840979786
Ken Ding <aenkil@gmail.com>
2016-11-13 15:10:32 +1100

    Create base App page with meta data

---
+++ app/pages/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+import Page from 'pages/Page';
+import AppContainer from 'containers/App';
+import { title, meta, link } from 'config/headAssets';
+
+const App = props => (
+  <Page title={title} meta={meta} link={link}>
+    <AppContainer {...props } />
+  </Page>
+);
+
+export default App;
+
