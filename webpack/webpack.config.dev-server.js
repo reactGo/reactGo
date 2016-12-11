64c770bd09786fea7b80273b8e8a35a54f9645ca
Ken Ding <aenkil@gmail.com>
2016-12-11 17:15:25 +1100

    Modify webpack dev to compile server-side node
    - Remove babel-node command from package.json
    - Remove unnecessary files in nodemon.json
    - Set node_modules as externals in result bundle

--- webpack/webpack.config.dev-server.js
+++ webpack/webpack.config.dev-server.js
@@ -1,38 +1,49 @@
 var path = require('path');
+var fs = require('fs');
 var webpack = require('webpack');
 var commonLoaders = require('./common.config').commonLoaders;
-var assetsPath = require('./common.config').assetsPath;
 var publicPath = require('./common.config').publicPath;
+
+var nodeModules = fs.readdirSync('node_modules')
+                    .filter(function(x) {
+                      return ['.bit'].indexOf(x) === -1;
+                    })
+                    .reduce(function(acc, cur) {
+                      acc[cur] = 'commonjs ' + cur;
+                      return acc;
+                    }, {});
 
 module.exports = {
     // The configuration for the server-side rendering
     name: 'server-side rendering',
     context: path.join(__dirname, '..', 'app'),
     entry: {
-      server: './server'
+      server: '../server/index'
     },
     target: 'node',
+    node: {
+      __dirname: false
+    },
     output: {
       // The output directory as absolute path
-      path: assetsPath,
+      path: path.join(__dirname, '..', 'compiled'),
       // The filename of the entry chunk as relative path inside the output.path directory
-      filename: 'server.js',
+      filename: '[name].dev.js',
       // The output path from the view of the Javascript
       publicPath: publicPath,
       libraryTarget: 'commonjs2'
     },
     module: {
-      loaders: commonLoaders.concat([
-           {
-              test: /\.css$/,
-              loader: 'css/locals?module&localIdentName=[name]__[local]___[hash:base64:5]'
-           }
-      ])
+      loaders: commonLoaders.concat({
+        test: /\.css$/,
+        loader: 'css/locals?module&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
+      })
     },
     resolve: {
       root: [path.join(__dirname, '..', 'app')],
       extensions: ['', '.js', '.jsx', '.css'],
     },
+    externals: nodeModules,
     plugins: [
         new webpack.DefinePlugin({
           __DEVCLIENT__: false,
