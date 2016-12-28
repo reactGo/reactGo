c231082e22c42c424ecb1334a061102364398ffd
Ken Ding <aenkil@gmail.com>
2016-12-28 17:11:27 +1100

    Move config/headAssets > pages/assets

--- app/pages/App.jsx
+++ app/pages/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Page from '../pages/Page';
 import AppContainer from '../containers/App';
-import { title, meta, link } from '../config/headAssets';
+import { title, meta, link } from './assets';
 
 const App = props => (
   <Page title={title} meta={meta} link={link}>
