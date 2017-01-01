cf5f7793ad79b0146981e66556471dde7b14fd9b
Ken Ding <aenkil@gmail.com>
2017-01-01 22:24:29 +1100

    Replace isDev with isProduction in config/app
    - Use isProduction boolean flag in pages/assets

--- config/app.js
+++ config/app.js
@@ -1,7 +1,7 @@
 export const HOST = process.env.HOSTNAME || 'localhost';
 export const PORT = process.env.PORT || '3000';
 
-export const isDev = () => __DEVCLIENT__ || __DEVSERVER__;
+export const isProduction = process.env.NODE_ENV === 'production';
 
 export const baseURL = `http://${HOST}:${PORT}`;
 // Replace with 'UA-########-#' or similar to enable tracking
