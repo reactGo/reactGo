ea11373d90bb883db8f66f6994c1e43aa33b935e
Ken Ding <aenkil@gmail.com>
2017-01-01 23:38:01 +1100

    Simplify export syntax for config/constants
    - Consistent with the rest of the application
    - More readable export syntax

--- server/config/constants.js
+++ server/config/constants.js
@@ -1,10 +1,6 @@
-/* Use this old export style until sequelize cli supports es6 syntax */
-function defaultExport() {}
-
-defaultExport.DB_TYPES = {
+export const DB_TYPES = {
   MONGO: 'MONGO',
   POSTGRES: 'POSTGRES',
   NONE: 'NONE'
 };
 
-module.exports = defaultExport;
