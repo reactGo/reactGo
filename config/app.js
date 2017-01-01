b7bc3232f4cef2b3ca00af419afb78095a24eda0
Ken Ding <aenkil@gmail.com>
2017-01-01 19:04:27 +1100

    Expose baseURL variable to server.jsx
    - Uppercase HOST and PORT
    - export baseURL to be used in server.jsx. Reduces server.jsx' responsibility of
      constructing the baseURL with a HOST and PORT

--- config/app.js
+++ config/app.js
@@ -1,6 +1,9 @@
-export const host = process.env.HOSTNAME || 'localhost';
-export const port = process.env.PORT || '3000';
+export const HOST = process.env.HOSTNAME || 'localhost';
+export const PORT = process.env.PORT || '3000';
+
 export const isDev = () => __DEVCLIENT__ || __DEVSERVER__;
+
+export const baseURL = `http://${HOST}:${PORT}`;
 // Replace with 'UA-########-#' or similar to enable tracking
 export const trackingID = null;
 
