e1425f9b47fd806da73f5284d0f4f0774fec46e8
Ken Ding <aenkil@gmail.com>
2017-01-01 18:57:24 +1100

    Change to cleaner export syntax in config/app

--- config/app.js
+++ config/app.js
@@ -1,13 +1,6 @@
-const host = process.env.HOSTNAME || 'localhost';
-const port = process.env.PORT || '3000';
-const isDev = () => __DEVCLIENT__ || __DEVSERVER__;
+export const host = process.env.HOSTNAME || 'localhost';
+export const port = process.env.PORT || '3000';
+export const isDev = () => __DEVCLIENT__ || __DEVSERVER__;
 // Replace with 'UA-########-#' or similar to enable tracking
-const trackingID = null;
-
-export {
-  host,
-  port,
-  trackingID,
-  isDev
-};
+export const trackingID = null;
 
