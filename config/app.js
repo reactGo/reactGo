336806b5c8e319f88b04481805465ae192866082
Ken Ding <aenkil@gmail.com>
2017-01-01 22:58:17 +1100

    Replace DEVCLIENT with isDebug and isClient in config
    - Reduce dependency on webpack to set __DEVCLIENT__ in DefinePlugin. Instead use
      isDebug and isClient defined in config/app to achieve the same outcome.

--- config/app.js
+++ config/app.js
@@ -2,6 +2,8 @@ export const HOST = process.env.HOSTNAME || 'localhost';
 export const PORT = process.env.PORT || '3000';
 
 export const isProduction = process.env.NODE_ENV === 'production';
+export const isDebug = process.env.NODE_ENV === 'development';
+export const isClient = typeof window !== 'undefined';
 
 export const baseURL = `http://${HOST}:${PORT}`;
 // Replace with 'UA-########-#' or similar to enable tracking
