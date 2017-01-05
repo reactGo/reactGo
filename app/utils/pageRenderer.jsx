4e85b891029bce2fdc028d7d1d833be1722a7b82
Ken Ding <aenkil@gmail.com>
2017-01-06 08:20:36 +1100

    Move tracking id to env (#795)
    * Expose GOOGLE_ANALYTICS_ID as env variable
    
    - Different environments (production, staging) might require GA to be different
    - Remove `analyticsScript` as arg in `buildPage`
    - Reorder script creation to be in order for readability
    
    * Change analytics snippet to async
    
    - Move to <head> as recommended. Although script tags in heads are not normally
      recommended, in this case it's a small snippet optimized by google + it helps
      track pageviews more accurately.
    
    * Rename createScriptTags -> createAppScript
    
    * Extract script creations to createScripts
    
    * 2.2.1
    
    * Update Changelog

--- app/utils/pageRenderer.jsx
+++ app/utils/pageRenderer.jsx
@@ -3,23 +3,7 @@ import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import { RouterContext } from 'react-router';
 import Helmet from 'react-helmet';
-import { trackingID } from '../../config/app';
-
-/*
- * Consider async script loading if you support IE9+
- * https://developers.google.com/analytics/devguides/collection/analyticsjs/
- */
-const createTrackingScript = trackingID =>
-`<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', ${trackingID}, 'auto');
-  ga('send', 'pageview');
-  </script>`;
-
-const analyticsScript = createTrackingScript(trackingID);
+import { createAppScript, createTrackingScript } from './createScripts';
 
 const createApp = (store, props) => renderToString(
   <Provider store={store}>
@@ -27,11 +11,7 @@ const createApp = (store, props) => renderToString(
   </Provider>
 );
 
-const createScriptTags = () => {
-  return `${analyticsScript}<script type="text/javascript" charset="utf-8" src="/assets/app.js"></script>`;
-};
-
-const buildPage = ({ componentHTML, initialState, headAssets, analyticsScript }) => {
+const buildPage = ({ componentHTML, initialState, headAssets }) => {
   return `
 <!doctype html>
 <html>
@@ -39,11 +19,12 @@ const buildPage = ({ componentHTML, initialState, headAssets, analyticsScript })
     ${headAssets.title.toString()}
     ${headAssets.meta.toString()}
     ${headAssets.link.toString()}
+    ${createTrackingScript()}
   </head>
   <body>
     <div id="app">${componentHTML}</div>
     <script>window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}</script>
-    ${createScriptTags()}
+    ${createAppScript()}
   </body>
 </html>`;
 };
@@ -52,6 +33,6 @@ export default (store, props) => {
   const initialState = store.getState();
   const componentHTML = createApp(store, props);
   const headAssets = Helmet.rewind();
-  return buildPage({ componentHTML, initialState, headAssets, analyticsScript });
+  return buildPage({ componentHTML, initialState, headAssets });
 };
 
--- config/env.js
+++ config/env.js
@@ -6,3 +6,5 @@ export const ENV = process.env.NODE_ENV || 'development';
 
 export const DB_TYPE = process.env.DB_TYPE || DB_TYPES.MONGO;
 
+export const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID || null;
+
