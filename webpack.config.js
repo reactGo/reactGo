8c49c79adbe57d23a3ed4ce207204a7caae464f3
Lap <lap@scn.org>
2017-01-09 00:15:34 -0800

    1) Upgraded Webpack to version 2.2.0-rc.3; upgraded dependencies to use this latest Webpack version.
    2) Refactored webpack.config.js for Webpack 2.  Also consolidated webpack/*.config.*.js files into a single webpack.config.js file.
    
    3) Added postcss.config.js as it is necessary for postcss-loader in Webpack 2.
    
    4) Modified karma.conf.js for Webpack 2.  Also moved karma.conf.js and tests.webpack.js to app/tests/ - may not be necessary but could be easier to fix the relative path lookup errors in the tests - the tests couldn't run before this migration and should be fixed in a different PR.
    
    5) Updated package.json and nodemon.json to reflect the Webpack 2 upgrade.
    
    6) This check-in does not introduce any new eslint errors.
    
    NOTE: engines.node is upgraded to ">=6.x" in package.json - which may not be necessary; but I am using node version 6.9.1 on my local development environment, so I don't have node 5.x to test.

---
+++ webpack.config.js
@@ -0,0 +1,234 @@
+/**
+ * webpack.config.js
+ *
+ * process.env.NODE_ENV is used to determine to return production config or not (an array with both browser and server config)
+ * if not, env is used to determine to return browser-rendering config (for hot module replacement) or server-side rendering config (for node)
+ * env is a string passed by "webpack --env" on command line or calling this function directly
+ * if env contains substring 'browser', then returns browser-rendering config, otherwise server-rendering config
+ *
+ * NOTE: browser/server is client/server-side rendering respectively in universial/isomophoric javascript
+ *
+ */
+const fs = require('fs');
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = (env = '') => {
+  const isProd = process.env.NODE_ENV === 'production';
+  const browserRender = (env.indexOf('browser') >= 0);
+  console.log(`running webpack.config.js: isProd=${isProd}; browserRender=${browserRender};`);
+
+  /**
+   * __dirname is changed after webpack-ed to another directory
+   * so process.cwd() is used instead to determine the correct base directory
+   */
+  const __DIR__ = process.cwd();
+
+  const PATHS = {
+    app: path.resolve(__DIR__, 'app'),
+    assets: path.resolve(__DIR__, 'public', 'assets'),
+    compiled: path.resolve(__DIR__, 'compiled'),
+    public: '/assets/', // use absolute path for css-loader?
+    modules: path.resolve(__DIR__, 'node_modules')
+  };
+
+  const externals = fs.readdirSync('node_modules')
+    .filter(x => ['.bin'].indexOf(x) === -1)
+    .reduce((acc, cur) => Object.assign(acc, { [cur]: 'commonjs ' + cur }), {});
+
+  const urlLoader = (limit = 10000) => ({
+    test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+    loader: 'url-loader',
+    query: { name: '[hash].[ext]', limit }, // name for file-loader
+    include: PATHS.app
+  });
+
+  const babelLoader = (production = false, browser = false) => {
+    const obj = {
+      test: /\.js$|\.jsx$/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react', 'stage-0'],
+        plugins: ['transform-decorators-legacy']
+      },
+      exclude: PATHS.modules
+    };
+    if (production) {
+      obj.query.plugins.push(
+        'transform-react-remove-prop-types',
+        'transform-react-constant-elements',
+        'transform-react-inline-elements'
+      );
+    } else if (browser) {
+      obj.query.presets.unshift('react-hmre');
+    }
+    return obj;
+  };
+
+  const cssLoader = (production = false, browser = false) => {
+    /*
+     * https://github.com/webpack/css-loader/issues/59 - use css-loader/locals on server-side rendering
+     * :local(.className) used to declare className in the local scope,
+     * local identifiers are exported by css-loader
+     *
+     * modules enable CSS Modules spec https://github.com/css-modules/css-modules
+     *
+     * importLoaders (int): That many loaders after the css-loader are applied to @import-ed resources.
+     *
+     */
+    const queryModules = 'modules';
+    const queryImportLoaders = 'importLoaders=1';
+    const queryLocal = 'localIdentName=[name]__[local]___[hash:base64:5]';
+
+    const queryStrExtractTextPlugin = '?' + queryModules + '&' + queryImportLoaders;
+    const queryStr = queryStrExtractTextPlugin + '&' + queryLocal;
+
+    const serverSide = 'css-loader/locals' + queryStr + '!postcss-loader';
+    const browserSide = 'style-loader!css-loader' + queryStr + '!postcss-loader';
+
+    const myLoader = browser ? browserSide : serverSide;
+
+    const prodBrowserRenderLoader = ExtractTextPlugin.extract({
+      fallbackLoader: 'style-loader',
+      loader: 'css-loader' + queryStrExtractTextPlugin + '!postcss-loader'
+    });
+    const loader = production && browser ? prodBrowserRenderLoader : myLoader;
+    const obj = { test: /\.css$/, include: PATHS.app, loader };
+    return obj;
+  };
+
+  const webpackPlugins = (production = false, browser = false) => {
+    const bannerOptions = { raw: true, banner: 'require("source-map-support").install();' };
+    const compress = { warnings: false };
+
+    if (!production && !browser) {
+      return [
+        new webpack.EnvironmentPlugin(['NODE_ENV']),
+        new webpack.IgnorePlugin(/vertx/),
+        new webpack.BannerPlugin(bannerOptions)
+      ];
+    }
+    if (!production && browser) {
+      return [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.EnvironmentPlugin(['NODE_ENV'])
+        // stylelint-webpack-plugin does not support webpack 2 yet
+        // new styleLintPlugin({
+        //   configFile: path.resolve(__DIR__, '.stylelintrc'),
+        //   context: path.resolve(__DIR__, 'app'),
+        //   files: '**/*.?(sa|sc|c)ss'
+        // })
+      ];
+    }
+    if (production && !browser) {
+      return [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.EnvironmentPlugin(['NODE_ENV']),
+        new webpack.IgnorePlugin(/vertx/),
+        new webpack.optimize.UglifyJsPlugin({ compress }),
+        new webpack.BannerPlugin(bannerOptions)
+      ];
+    }
+    if (production && browser) {
+      const filename = 'styles/main.css';
+      const allChunks = true;
+
+      return [
+        new ExtractTextPlugin({ filename, allChunks }), // extracted css to css/main.css
+        // new webpack.optimize.CommonsChunkPlugin({ names: ['vendor', 'manifest'] }),
+        new webpack.optimize.UglifyJsPlugin({ compress }),
+        new webpack.EnvironmentPlugin(['NODE_ENV'])
+      ];
+    }
+    return [];
+  };
+
+  const hotMiddlewareScript = 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true';
+
+  const resolve = {
+    modules: [PATHS.app, PATHS.modules],
+    extensions: ['.js', '.jsx', '.css'],
+  };
+
+  /*
+   * PRODUCTION WEBPACK CONFIG
+   */
+  const prodServerRender = {
+    devtool: 'source-map',
+    context: PATHS.app,
+    entry: { server: '../server/index' },
+    target: 'node',
+    node: { __dirname: true, __filename: true },
+    externals,
+    output: {
+      path: PATHS.compiled,
+      filename: '[name].js',
+      publicPath: PATHS.public,
+      libraryTarget: 'commonjs2'
+    },
+    module: { rules: [babelLoader(isProd, false), cssLoader(isProd, false), urlLoader()] },
+    resolve,
+    plugins: webpackPlugins(true, false)
+  };
+  const prodBrowserRender = {
+    devtool: 'cheap-module-source-map',
+    context: PATHS.app,
+    entry: { app: ['./client'] },
+    node: { __dirname: true, __filename: true },
+    output: {
+      path: PATHS.assets,
+      filename: '[name].js', // filename: '[name].[hash:6].js',
+      chunkFilename: '[name].[chunkhash:6].js', // for code splitting. will work without but useful to set
+      publicPath: PATHS.public
+    },
+    module: { rules: [babelLoader(isProd, true), cssLoader(isProd, true), urlLoader()] },
+    resolve,
+    plugins: webpackPlugins(true, true)
+  };
+
+
+  /*
+   * DEVELOPMENT WEBPACK CONFIG
+   */
+  const devBrowserRender = {
+    devtool: 'eval',
+    context: PATHS.app,
+    entry: { app: ['./client', hotMiddlewareScript] },
+    node: { __dirname: true, __filename: true },
+    output: {
+      path: PATHS.assets,
+      filename: '[name].js',
+      publicPath: PATHS.public
+    },
+    module: { rules: [babelLoader(isProd, true), cssLoader(isProd, true), urlLoader()] },
+    resolve,
+    plugins: webpackPlugins(false, true)
+  };
+
+  const devServerRender = {
+    devtool: 'sourcemap',
+    context: PATHS.app,
+    entry: { server: '../server/index' },
+    target: 'node',
+    node: { __dirname: true, __filename: true },
+    externals,
+    output: {
+      path: PATHS.compiled,
+      filename: '[name].dev.js',
+      publicPath: PATHS.public,
+      libraryTarget: 'commonjs2',
+    },
+    module: { rules: [babelLoader(isProd, false), cssLoader(isProd, false), urlLoader()] },
+    resolve,
+    plugins: webpackPlugins(false, false)
+  };
+
+  const prodConfig = [prodBrowserRender, prodServerRender];
+  const devConfig = browserRender ? devBrowserRender : devServerRender;
+  const configuration = isProd ? prodConfig : devConfig;
+
+  // console.log('%o', configuration);
+  return configuration;
+};
