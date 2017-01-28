9fcfa126b6681449338fdfc671387c99c93cc1a4
Klemen Strusnik <klemen.strusnik@gmail.com>
2017-01-28 15:16:15 +0100

    add comment about supported db types in env

--- config/env.js
+++ config/env.js
@@ -4,7 +4,9 @@ export const HOST = process.env.HOSTNAME || 'localhost';
 export const PORT = process.env.PORT || '3000';
 export const ENV = process.env.NODE_ENV || 'development';
 
-export const DB_TYPE = process.env.DB_TYPE || DB_TYPES.MONGO;
+
+export const DB_TYPE = process.env.DB_TYPE || DB_TYPES.MONGO; // Supported: DB_TYPES.MONGO|POSTGRES|NONE
+
 
 export const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID || null;
 
