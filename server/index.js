33e4d27964fb9998a1d76a01b605d3fee3687f85
Ken Ding <aenkil@gmail.com>
2017-02-06 00:34:59 +1100

    Move webpack requires into isDebug conditional
    - We do not want to require unnecessary code for production bundle

--- server/index.js
+++ server/index.js
@@ -7,10 +7,6 @@ import initExpress from './init/express';
 import initRoutes from './init/routes';
 import renderMiddleware from './render/middleware';
 
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const webpackConfig = require('../webpack/webpack.config');
-
 const app = express();
 
 /*
@@ -27,6 +23,9 @@ initPassport();
 
 if (isDebug) {
   // enable webpack hot module replacement
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const webpackConfig = require('../webpack/webpack.config');
   const devBrowserConfig = webpackConfig('browser');
   const compiler = webpack(devBrowserConfig);
   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: devBrowserConfig.output.publicPath }));
