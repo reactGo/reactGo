725e630683ee265b43bc660660d54408482e1b72
Ken Ding <aenkil@gmail.com>
2017-02-05 11:16:41 +1100

    Rename __DIR__ to CURRENT_WORKING_DIRECTORY
    Although __DIR__ is pretty clear, CURRENT_WORKING_DIRECTORY gives more context

--- webpack/paths.js
+++ webpack/paths.js
@@ -1,16 +1,17 @@
 const path = require('path');
 
-/**
+/*
  * __dirname is changed after webpack-ed to another directory
  * so process.cwd() is used instead to determine the correct base directory
+ * Read more: https://nodejs.org/api/process.html#process_process_cwd
  */
-const __DIR__ = process.cwd();
+const CURRENT_WORKING_DIR = process.cwd();
 
 module.exports = {
-  app: path.resolve(__DIR__, 'app'),
-  assets: path.resolve(__DIR__, 'public', 'assets'),
-  compiled: path.resolve(__DIR__, 'compiled'),
+  app: path.resolve(CURRENT_WORKING_DIR, 'app'),
+  assets: path.resolve(CURRENT_WORKING_DIR, 'public', 'assets'),
+  compiled: path.resolve(CURRENT_WORKING_DIR, 'compiled'),
   public: '/assets/', // use absolute path for css-loader?
-  modules: path.resolve(__DIR__, 'node_modules')
+  modules: path.resolve(CURRENT_WORKING_DIR, 'node_modules')
 };
 
