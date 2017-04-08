be6eceefa68fc377ebe2a6b8f6fd1275f269b632
ZeroCho <zerohch0@gmail.com>
2017-04-08 23:21:32 +0900

    Update: react 15.5.0

--- app/components/MainSection.jsx
+++ app/components/MainSection.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import TopicItem from '../components/TopicItem';
 import styles from '../css/components/main-section';
