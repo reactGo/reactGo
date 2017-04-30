5ddb4950d42d7ab98cbbb5117b357d2554924cc2
Ken Ding <aenkil@gmail.com>
2017-04-30 21:37:54 +1000

    Modify css filename to be contenthash for long-term caching

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -35,7 +35,7 @@ module.exports = ({ production = false, browser = false } = {}) => {
       new webpack.EnvironmentPlugin(['NODE_ENV']),
       new webpack.DefinePlugin(compileTimeConstantForMinification),
       new ExtractTextPlugin({
-        filename: 'styles/main.css',
+        filename: '[contenthash].css',
         allChunks: true
       }),
       new webpack.optimize.UglifyJsPlugin({ compress }),
