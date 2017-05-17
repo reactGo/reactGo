92bd9ef039e52b91a9db6d119249bdfe2d101492
Joe Motacek <jmotacek@bp-3.com>
2017-05-17 14:01:55 -0500

    Removed modules from Babel loader preset

--- webpack/rules/javascript.js
+++ webpack/rules/javascript.js
@@ -1,17 +1,17 @@
 const PATHS = require('../paths');
 
 module.exports = ({ production = false, browser = false } = {}) => {
-  const presets = ['es2015', 'react', 'stage-0'];
+  const presets = [['es2015', { modules: false }], 'react', 'stage-0'];
   const plugins = production ? [
       'transform-react-remove-prop-types',
       'transform-react-constant-elements',
       'transform-react-inline-elements'
-  ]: [];
+  ] : [];
 
   if (!production && browser) {
     plugins.push('react-hot-loader/babel');
   }
-  
+
   return {
     test: /\.js$|\.jsx$/,
     loader: 'babel-loader',
