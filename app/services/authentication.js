a9107ca4f34fba5dfda9b7d536f6245da6a6f599
ZeroCho <zerohch0@gmail.com>
2020-07-06 18:33:18 +0900

    Update: services to typescript

--- app/services/authentication.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default () => {
-  return {
-    login: ({ email, password }) => axios.post('/sessions', { email, password }),
-    signUp: ({ email, password }) => axios.post('/users', { email, password }),
-    logOut: () => axios.delete('/sessions')
-  };
-};
