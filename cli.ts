ebbb13469e3037478b6d80001d3db9f2b82e1b83
ZeroCho <zerohch0@gmail.com>
2020-08-11 18:07:50 +0900

    Update: dev mode cli

--- cli.ts
+++ cli.ts
@@ -1,20 +1,26 @@
 #!/usr/bin/env node
 import * as fs from 'fs';
 import * as inquirer from 'inquirer';
+import { program } from 'commander';
 import * as path from 'path';
 import * as shell from 'shelljs';
+import { version } from './package.json';
 
 const copyRecursiveSync = (src: string, dest: string) => {
-  const exists = fs.existsSync(src);
-  const stats = exists && fs.statSync(src);
-  const isDirectory = stats && stats.isDirectory();
-  if (isDirectory) {
-    fs.mkdirSync(dest);
-    fs.readdirSync(src).forEach((childItemName) => {
-      copyRecursiveSync(path.join(src, childItemName), path.join(dest, childItemName));
-    });
-  } else {
-    fs.copyFileSync(src, dest);
+  try {
+    const exists = fs.existsSync(src);
+    const stats = exists && fs.statSync(src);
+    const isDirectory = stats && stats.isDirectory();
+    if (isDirectory) {
+      fs.mkdirSync(dest);
+      fs.readdirSync(src).forEach((childItemName) => {
+        copyRecursiveSync(path.join(src, childItemName), path.join(dest, childItemName));
+      });
+    } else {
+      fs.copyFileSync(src, dest);
+    }
+  } catch (err) {
+    console.error(err);
   }
 };
 
@@ -30,26 +36,144 @@ const removeLines = (src: string, lines: number[] = []) => {
   });
 };
 
-inquirer
-  .prompt([{
-    name: 'db',
-    message: 'Choose database you want to use.',
-    type: 'list',
-    choices: ['MySQL', 'PostgreSQL', 'MongoDB', 'none'],
-  }, {
-    name: 'front',
-    message: 'Choose state management framework you want to use.',
-    type: 'list',
-    choices: ['redux + thunk', 'redux + saga', 'mobx'],
-  }])
-  .then((answers) => {
-    console.log(answers);
-    console.log(answers.db);
-    console.log(process.cwd());
-    const list = ['config', 'types', 'webpack', 'app', 'server', 'package.json', '.sequelizerc', 'nodemon.json', '.babelrc', '.gitignore', '.eslintrc.json', '.eslintignore', 'Procfile', 'tsconfig.json'];
-    list.forEach((name) => {
-      copyRecursiveSync(path.join(__dirname, name), path.join(process.cwd(), name));
+let removeTargetModules: string[] = [];
+const mobxSpecificModules = ['mobx', 'mobx-react', 'mobx-react-router'];
+const reduxSpecificModules = ['redux', 'redux-logger', '@types/redux-logger', 'redux-devtools-extension'];
+const thunkSpecificModules = ['redux-thunk'];
+const sagaSpecificModules = ['redux-saga'];
+const mongoSpecificModules = ['@types/mongodb', 'mongoose', '@types/mongoose', 'connect-mongo'];
+const pgSpecificModules = ['sequelize', 'sequelize-cli', '@types/pg', 'pg', 'pg-hstore', 'connect-pg-simple', '@types/connect-pg-simple'];
+const mysqlSpecificModules = ['sequelize', 'sequelize-cli', '@types/pg', 'mysql2', 'pg-hstore', 'express-mysql-session', '@types/express-mysql-session'];
+
+program
+  .version(version)
+  .option('-d --dev', 'development mode')
+  .parse(process.argv);
+
+if (program.dev) {
+  console.log('running dev mode CLI');
+  inquirer
+    .prompt([{
+      name: 'db',
+      message: 'Choose database you want to use.',
+      type: 'list',
+      choices: ['MySQL', 'PostgreSQL', 'MongoDB', 'none'],
+    }, {
+      name: 'front',
+      message: 'Choose state management framework you want to use.',
+      type: 'list',
+      choices: ['redux + thunk', 'redux + saga', 'mobx'],
+    }])
+    .then((answers) => {
+      let front;
+      switch (answers.front) {
+        case 'redux + saga':
+          front = 'app_saga';
+          removeTargetModules = removeTargetModules.concat(mobxSpecificModules).concat(thunkSpecificModules);
+          break;
+        case 'mobx':
+          front = 'app_mobx';
+          removeTargetModules = removeTargetModules.concat(reduxSpecificModules).concat(sagaSpecificModules).concat(thunkSpecificModules);
+          break;
+        default: // thunk
+          front = 'app_thunk';
+          removeTargetModules = removeTargetModules.concat(mobxSpecificModules).concat(sagaSpecificModules);
+          break;
+      }
+      let db;
+      switch (answers.db) {
+        case 'MySQL':
+          db = 'server_mysql';
+          removeTargetModules = removeTargetModules.concat(pgSpecificModules).concat(mongoSpecificModules);
+          break;
+        case 'PostgreSQL':
+          db = 'server_pg';
+          removeTargetModules = removeTargetModules.concat(mysqlSpecificModules).concat(mongoSpecificModules);
+          break;
+        case 'none':
+          db = 'server_none';
+          removeTargetModules = removeTargetModules.concat(pgSpecificModules).concat(mongoSpecificModules).concat(mysqlSpecificModules);
+          break;
+        default: // mongo
+          db = 'server_mongo';
+          removeTargetModules = removeTargetModules.concat(pgSpecificModules).concat(mysqlSpecificModules);
+          break;
+      }
+      let exists = fs.existsSync(path.join(__dirname, 'server'));
+      if (exists) {
+        fs.unlinkSync(path.join(__dirname, 'server'));
+      }
+      exists = fs.existsSync(path.join(__dirname, 'app'));
+      if (exists) {
+        fs.unlinkSync(path.join(__dirname, 'app'));
+      }
+      fs.symlinkSync(path.join(__dirname, db), path.join(__dirname, 'server'), 'dir');
+      fs.symlinkSync(path.join(__dirname, front), path.join(__dirname, 'app'), 'dir');
+      console.log('installing node modules...');
+      shell.exec('npm install');
+      shell.exec(`npm rm ${removeTargetModules.join(' ')}`);
+      console.log('done');
     });
-    shell.exec('npm install');
-    console.log('done');
-  });
+} else {
+  inquirer
+    .prompt([{
+      name: 'db',
+      message: 'Choose database you want to use.',
+      type: 'list',
+      choices: ['MySQL', 'PostgreSQL', 'MongoDB', 'none'],
+    }, {
+      name: 'front',
+      message: 'Choose state management framework you want to use.',
+      type: 'list',
+      choices: ['redux + thunk', 'redux + saga', 'mobx'],
+    }])
+    .then((answers) => {
+      console.log(answers);
+      console.log(answers.db);
+      console.log(process.cwd());
+      let front;
+      switch (answers.front) {
+        case 'redux + saga':
+          front = 'app_saga';
+          removeTargetModules = removeTargetModules.concat(mobxSpecificModules).concat(thunkSpecificModules);
+          break;
+        case 'mobx':
+          front = 'app_mobx';
+          removeTargetModules = removeTargetModules.concat(reduxSpecificModules).concat(sagaSpecificModules).concat(thunkSpecificModules);
+          break;
+        default: // thunk
+          front = 'app_thunk';
+          removeTargetModules = removeTargetModules.concat(mobxSpecificModules).concat(sagaSpecificModules);
+          break;
+      }
+      let db;
+      switch (answers.db) {
+        case 'MySQL':
+          db = 'server_mysql';
+          removeTargetModules = removeTargetModules.concat(pgSpecificModules).concat(mongoSpecificModules);
+          break;
+        case 'PostgreSQL':
+          db = 'server_pg';
+          removeTargetModules = removeTargetModules.concat(mysqlSpecificModules).concat(mongoSpecificModules);
+          break;
+        case 'none':
+          db = 'server_none';
+          removeTargetModules = removeTargetModules.concat(pgSpecificModules).concat(mongoSpecificModules).concat(mysqlSpecificModules);
+          break;
+        default: // mongo
+          db = 'server_mongo';
+          removeTargetModules = removeTargetModules.concat(pgSpecificModules).concat(mysqlSpecificModules);
+          break;
+      }
+      copyRecursiveSync(path.join(__dirname, db), path.join(process.cwd(), 'server'));
+      copyRecursiveSync(path.join(__dirname, front), path.join(process.cwd(), 'app'));
+      const list = ['config', 'types', 'webpack', 'package.json', '.sequelizerc', 'nodemon.json', '.babelrc', '.gitignore', '.eslintrc.json', '.eslintignore', 'Procfile', 'tsconfig.json'];
+      list.forEach((name) => {
+        copyRecursiveSync(path.join(__dirname, name), path.join(process.cwd(), name));
+      });
+      console.log('installing node modules...');
+      shell.exec('npm install');
+      shell.exec(`npm rm ${removeTargetModules.join(' ')}`);
+      console.log('done');
+    });
+}
